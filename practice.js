6409cea43456c55cb238ee4234398ca057564898
Sushil97 <sushil500530@gmail.com>
2023-08-04 13:38:21 +0600

    practice problems used function

---
+++ practice.js
@@ -0,0 +1,7 @@
+// mile theke kilometer ber korar program 
+function mileToKilo(mile){
+    let kilo = mile * 1.61;
+    return kilo;
+}
+let mileAvar = 20;
+console.log(mileToKilo(mileAvar), "KM");
